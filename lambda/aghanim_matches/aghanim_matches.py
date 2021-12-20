2250bb161be86c198817932392a1b9bb25d8c40f
unknown <alyu@ucsd.edu>
2021-12-19 16:34:51 -0800

    implemented table put for aghs lambda

--- lambda/aghanim_matches/aghanim_matches.py
+++ lambda/aghanim_matches/aghanim_matches.py
@@ -6,7 +6,6 @@ from api_caller.lib import APICaller, API_Call_Metadata
 
 #Create clients for interfacting with AWS resources
 client = boto3.resource('dynamodb')
-players_table = client.Table(os.environ['PLAYERS_TABLE'])
 api_calls_table = client.Table(os.environ['API_CALLS_TABLE'])
 
 #Load metadata for logging
@@ -17,6 +16,8 @@ metadata = API_Call_Metadata(FUNC_NAME, LOG_GROUP, LOG_STREAM)
 
 with open('query.txt', 'r') as f:
     query = f.read()
+
+aghs_matches_table = client.Table(os.environ['AGHANIM_MATCHES_TABLE'])
 
 def handler(event, context):
     print('request: {}'.format(json.dumps(event)))
@@ -32,14 +33,9 @@ def handler(event, context):
     #Make API call
     api_caller = APICaller(api_calls_table)
     data = api_caller.query(query, variables, metadata)
-    players = data['data']['leaderboard']['season']
+    matches = data['data']['stratz']['page']['aghanim']['matches']
 
-    #Without batch writing: ~3200 entires in 10m
-    #With batch writing: ~5200 entries in 10m
-    with players_table.batch_writer() as batch:
-        for player in players:
-            #Check if the player existsin the table already
-            #table.get_item(Key={pkey: player['steamAccountId']})
-            #TODO: Batch get items, and only write if the item doesn't exist
-            item = {pkey: player['steamAccountId'], 'region': region}
-            batch.put_item(Item=item)
+
+    with aghs_matches_table.batch_writer() as batch:
+        for match in matches:
+            batch.put_item(Item=match)

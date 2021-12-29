86d38703b056eeb63e81acfce2938c3e192b02d9
unknown <alyu@ucsd.edu>
2021-12-28 18:49:41 -0800

    drop tables recursively

--- lambda/rds_initializer/index.py
+++ lambda/rds_initializer/index.py
@@ -24,13 +24,13 @@ def handler(event, context):
     )
     cur = conn.cursor()
 
-    # #Destroy existing tables
-    # cur.execute("""DROP TABLE IF EXISTS matches;""")
-    # cur.execute("""DROP TABLE IF EXISTS players;""")
-    # cur.execute("""DROP TABLE IF EXISTS playerDepthList;""")
-    # cur.execute("""DROP TABLE IF EXISTS playerBlessings;""")
-    # cur.execute("""DROP TABLE IF EXISTS depthList;""")
-    # cur.execute("""DROP TABLE IF EXISTS ascenionAbilities;""")
+    #Destroy existing tables
+    cur.execute("""DROP TABLE IF EXISTS matches CASCADE;""")
+    cur.execute("""DROP TABLE IF EXISTS players CASCADE;""")
+    cur.execute("""DROP TABLE IF EXISTS playerDepthList CASCADE;""")
+    cur.execute("""DROP TABLE IF EXISTS playerBlessings CASCADE;""")
+    cur.execute("""DROP TABLE IF EXISTS depthList CASCADE;""")
+    cur.execute("""DROP TABLE IF EXISTS ascenionAbilities CASCADE;""")
 
     #Get all tables
     cur.execute("""SELECT table_name
